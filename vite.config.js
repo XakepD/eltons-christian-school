2c449db3bc9a76a58adcb540695fc54606b6a344
XakepD <dahsec18@gmail.com>
2025-04-28 02:35:08 +0100

    host3

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Allows the server to be accessible externally
-    port: 3000, // Default port for the development server
-    strictPort: true, // Ensures the server uses the specified port
-    proxy: {
-      "/api": {
-        target: "https://eltonschristianschool.onrender.com", // Proxy API requests to the backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: true,
+    allowedHosts: true, // Allows all hosts
   },
 })
